fbd12353b079973d76368582479440ca4ec447ed
Sameer Jha <sameerjha236@gmail.com>
2023-05-06 02:29:47 +0530

    base layout set

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
-
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Portfolio from "./Components/Portfolio";
+import Skills from "./Components/Skills";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <About />
+      <Contact />
+      <Portfolio />
+      <Skills />
+    </div>
+  );
 }
 
 export default App;
