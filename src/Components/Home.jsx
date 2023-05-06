6cae0f02be189c902d481ccc7a825075bce82b11
Sameer Jha <sameerjha236@gmail.com>
2023-05-07 02:44:54 +0530

    Navbar updated 01

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="home">
+      <p>
+        Hello, I'm Sameer Jha - a web developer with a passion for creating
+        exceptional websites that deliver great user experiences. I'm always
+        learning new skills and technologies to improve my craft.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,42 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleToggleClick = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <nav className="navbar">
+      <div className="nav-header">
+        <Link to="/">
+          <p className="logo">Sameer Jha</p>
+        </Link>
+        <div className="toggle">
+          <button className="toggle-btn" onClick={handleToggleClick}>
+            Click me
+          </button>
+        </div>
+      </div>
+      <div className={`nav-links ${showLinks ? "show" : ""}`}>
+        <ul className="links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
