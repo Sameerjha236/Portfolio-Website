668f972fdb4d4783f3a407823d5e81cd5ca0af4b
Sameer Jha <sameerjha236@gmail.com>
2023-05-07 16:46:19 +0530

    Navbar Hover working started

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,10 +2,11 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home">
+      <h4>Hello, I'm</h4> <h1>Sameer Jha</h1>
       <p>
-        Hello, I'm Sameer Jha - a web developer with a passion for creating
-        exceptional websites that deliver great user experiences. I'm always
-        learning new skills and technologies to improve my craft.
+        a web developer with a passion for creating exceptional websites that
+        deliver great user experiences. I'm always learning new skills and
+        technologies to improve my craft.
       </p>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import { BiHomeAlt2 } from "react-icons/bi";
+import { RxAvatar } from "react-icons/rx";
+import { FaEnvelope } from "react-icons/fa";
+import { HiRocketLaunch } from "react-icons/hi2";
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
 
@@ -22,16 +25,24 @@ const Navbar = () => {
       <div className={`nav-links ${showLinks ? "show" : ""}`}>
         <ul className="links">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" data-text="Home">
+              <BiHomeAlt2 className="icon" />
+            </Link>
           </li>
           <li>
-            <Link to="/portfolio">Portfolio</Link>
+            <Link to="/portfolio" data-text="Portfolio">
+              <HiRocketLaunch className="icon" />
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" data-text="Contact">
+              <FaEnvelope className="icon" />
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" data-text="About">
+              <RxAvatar className="icon" />
+            </Link>
           </li>
         </ul>
       </div>
