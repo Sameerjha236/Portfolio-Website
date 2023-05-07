10665269f160559949b448240367d2f6ef45ba86
Sameer Jha <sameerjha236@gmail.com>
2023-05-08 01:51:12 +0530

    Home Page working started

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Sameer from ".././Assests/Sameer_Jha_Resume.pdf";
+import Typewriter from "typewriter-effect";
 const Home = () => {
   return (
     <div className="home">
-      <h4>Hello, I'm</h4> <h1>Sameer Jha</h1>
-      <p>
-        a web developer with a passion for creating exceptional websites that
-        deliver great user experiences. I'm always learning new skills and
-        technologies to improve my craft.
-      </p>
+      <div className="hero">
+        <h4>Hello, I'm</h4>
+        <h1>
+          <Typewriter
+            options={{
+              strings: ["Sameer Jha"],
+              autoStart: true,
+              loop: true,
+            }}
+          />{" "}
+        </h1>
+        <p>
+          A web developer with a passion for creating exceptional websites that
+          deliver great user experiences. I'm always learning new skills and
+          technologies to improve my craft.
+        </p>
+        <div className="btn-sect">
+          <a href={Sameer} className="btn" download>
+            Download CV
+          </a>
+
+          <NavLink to="/contact" className="btn">
+            Let's Talk
+          </NavLink>
+        </div>
+      </div>
+      <div className="something"></div>
     </div>
   );
 };
