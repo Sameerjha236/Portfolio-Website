7ee516552c61ae413686b17645ccfb038d199579
Sameer Jha <sameerjha236@gmail.com>
2023-05-18 01:54:56 +0530

    Navbar almost complete

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { BiHomeAlt2 } from "react-icons/bi";
 import { RxAvatar } from "react-icons/rx";
 import { FaEnvelope } from "react-icons/fa";
 import { HiRocketLaunch } from "react-icons/hi2";
 import { FaBars } from "react-icons/fa";
-const Navbar = () => {
+const Nav = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
-    <nav className={`navbar ${showLinks ? "show" : ""}`}>
+    <nav className="navbar">
       <div className="nav-header">
         <NavLink to="/">
           <p className="logo">Sameer Jha</p>
         </NavLink>
-        <FaBars className="nav-toggle" onClick={toggleLinks} />
+        <button className="nav-toggle" onClick={toggleLinks}>
+          <FaBars />
+        </button>
       </div>
-      <div className={`links-container ${showLinks ? "show" : ""}`}>
-        <ul className={`links ${showLinks ? "show" : ""}`}>
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
           <li>
             <NavLink to="/" data-text="Home">
               <BiHomeAlt2 className="icon" />
@@ -46,4 +59,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Nav;
