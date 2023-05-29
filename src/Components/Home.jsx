7bf6515b48a8a43e7bc355f9da0b75d59d0d5713
Sameer Jha <sameerjha236@gmail.com>
2023-05-29 23:38:47 +0530

    Home updated and Loading Page completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect } from "react";
 import Loading from "./Loading";
 import { NavLink } from "react-router-dom";
+import { FaInstagram, FaGithub } from "react-icons/fa";
 import Sameer from ".././Assests/Sameer_Jha_Resume.pdf";
 import Typewriter from "typewriter-effect";
-// import profile from "../Assests/profile_full.JPEG";
+
 const Home = ({ isLoading, setIsLoading }) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
-    }, 600);
+    }, 800);
   }, [setIsLoading]);
+
   if (isLoading) return <Loading />;
+
   return (
     <div className="home">
       <div className="hero">
         <h4>Hello, I'm</h4>
         <h1>
           <Typewriter
+            className="typo"
             options={{
               strings: ["Sameer Jha"],
               autoStart: true,
@@ -27,15 +31,31 @@ const Home = ({ isLoading, setIsLoading }) => {
           />
         </h1>
         <p>
-          A web developer with a passion for creating exceptional websites that
+          A Web Developer with a passion for creating exceptional websites that
           deliver great user experiences. I'm always learning new skills and
           technologies to improve my craft.
         </p>
+        <div className="links-home">
+          <ul>
+            <li>
+              <a
+                href="https://www.instagram.com/sameerjha23/"
+                className="instagram"
+              >
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/Sameerjha236" className="github">
+                <FaGithub />
+              </a>
+            </li>
+          </ul>
+        </div>
         <div className="btn-sect">
           <a href={Sameer} className="btn" download>
             Download CV
           </a>
-
           <NavLink to="/contact" className="btn">
             Let's Talk
           </NavLink>
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const Loading = () => {
-  return <div className="Loading">Loading</div>;
+  const [randomQuestion, setRandomQuestion] = useState("");
+  useEffect(() => {
+    const companyQuestions = [
+      "What is the mission statement of your company?",
+      "How long has your company been in business?",
+      "What are the core values of your company?",
+      "What sets your company apart from competitors?",
+      "Can you briefly describe the products or services your company offers?",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * companyQuestions.length);
+    const selectedQuestion = companyQuestions[randomIndex];
+
+    setRandomQuestion(selectedQuestion);
+  }, []);
+
+  return (
+    <div className="Loading">
+      <h3>{randomQuestion}</h3>
+    </div>
+  );
 };
 
 export default Loading;
