056dbeb0a6b8a7034eeae8ab759ac1a780bd797b
Sameer Jha <sameerjha236@gmail.com>
2023-05-31 03:50:16 +0530

    Portfolio Page Updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+
+import { FaGithub } from "react-icons/fa";
 import Loading from "./Loading";
 
 const Portfolio = ({ isLoading, setIsLoading }) => {
@@ -8,13 +10,53 @@ const Portfolio = ({ isLoading, setIsLoading }) => {
       setIsLoading(false);
     }, 1000);
   }, [setIsLoading]);
+
   if (isLoading) return <Loading />;
+
   return (
     <div className="portfolio">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni sed culpa
-        officia.
-      </p>
+      <div className="header">
+        <h4>My Recent</h4>
+        <h3>Work</h3>
+      </div>
+      <div className="card-section ">
+        <div className="card">
+          <img
+            className="project-pic"
+            src="https://user-images.githubusercontent.com/90266216/218820683-2ea0b519-e82c-4533-98c1-686188d65264.png"
+            alt="Card 1"
+          />
+          <div className="card-overlay">
+            <h5>CarWale</h5>
+            <p>
+              Carwale is a web app that offers personalized car recommendations
+              and price predictions to its users.
+            </p>
+            <a
+              className="github"
+              href="https://github.com/Sameerjha236/CarWale"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="image2.jpg" className="project-pic" alt="Card 2" />
+          <div className="card-overlay">
+            <h5>Portfolio Website</h5>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+              sequi praesentium iste.
+            </p>
+            <a
+              className="github"
+              href="https://github.com/Sameerjha236/CarWale"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
