e4ad427fec1a17d795c59c62298fdf3e1cba4c6e
Sameer Jha <sameerjha236@gmail.com>
2023-10-10 04:48:52 +0530

    skill page updated

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -18,50 +18,52 @@ const Skills = ({ isLoading, setIsLoading }) => {
         <h3>My Experince</h3>
       </div>
       <div className="card-section">
-        <div className="card">
-          <h3>Frontend Development</h3>
-          <div className="skills-list">
-            <p className="item">
-              <FaHtml5 />
-              <h4>HTML</h4>
-            </p>
-            <p className="item">
-              <FaCss3 />
-              <h4>CSS</h4>
-            </p>
-            <p className="item">
-              <FaJs />
-              <h4>JavaScript</h4>
-            </p>
-            <p className="item">
-              <FaReact />
-              <h4>React</h4>
-            </p>
-            <p className="item">
-              <FaBootstrap />
-              <h4>Bootstrap</h4>
-            </p>
+        <div className="pair">
+          <div className="card">
+            <h3>Frontend Development</h3>
+            <div className="skills-list">
+              <p className="item">
+                <FaHtml5 />
+                <h4>HTML</h4>
+              </p>
+              <p className="item">
+                <FaCss3 />
+                <h4>CSS</h4>
+              </p>
+              <p className="item">
+                <FaJs />
+                <h4>JavaScript</h4>
+              </p>
+              <p className="item">
+                <FaReact />
+                <h4>React</h4>
+              </p>
+              <p className="item">
+                <FaBootstrap />
+                <h4>Bootstrap</h4>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <h3>Backend Development</h3>
-          <div className="skills-list">
-            <p className="item">
-              <SiPython />
-              <h4>Python</h4>
-            </p>
-            <p className="item">
-              <IoLogoNodejs />
-              <h4>Node.js</h4>
-            </p>
-            <p className="item">
-              <SiExpress />
-              <h4>Express.js</h4>
-            </p>
-            <p className="item">
-              <SiMysql />
-              <h4>MySQL</h4>
-            </p>
+          <div className="card">
+            <h3>Backend Development</h3>
+            <div className="skills-list">
+              <p className="item">
+                <SiPython />
+                <h4>Python</h4>
+              </p>
+              <p className="item">
+                <IoLogoNodejs />
+                <h4>Node.js</h4>
+              </p>
+              <p className="item">
+                <SiExpress />
+                <h4>Express.js</h4>
+              </p>
+              <p className="item">
+                <SiMysql />
+                <h4>MySQL</h4>
+              </p>
+            </div>
           </div>
         </div>
       </div>
