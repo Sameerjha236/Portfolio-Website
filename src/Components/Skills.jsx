810fc3fa989b9879525bb46babcd4bf5e66281aa
Sameer Jha <sameerjha236@gmail.com>
2024-03-19 00:37:40 +0530

    Skill.jsx updated

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -18,56 +18,60 @@ const Skills = ({ isLoading, setIsLoading }) => {
         <h3>My Experince</h3>
       </div>
       <div className="card-section">
-        <div className="pair">
-          <div className="card">
-            <h3>Frontend Development</h3>
-            <div className="skills-list">
-              <p className="item">
-                <FaReact />
-                <h4>React</h4>
-              </p>
-              <p className="item">
-                <FaJs />
-                <h4>JavaScript</h4>
-              </p>
-              <p className="item">
-                <FaBootstrap />
-                <h4>Bootstrap</h4>
-              </p>
-              <p className="item">
-                <SiTailwindcss />
-                <h4>Tailwind</h4>
-              </p>
-              <p className="item">
-                <FaHtml5 />
-                <h4>HTML</h4>
-              </p>
-              <p className="item">
-                <FaCss3 />
-                <h4>CSS</h4>
-              </p>
-            </div>
+        <div
+          className="card"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <h3>Frontend Development</h3>
+          <div className="skills-list">
+            <p className="item">
+              <FaReact />
+              <h4>React</h4>
+            </p>
+            <p className="item">
+              <FaJs />
+              <h4>JavaScript</h4>
+            </p>
+            <p className="item">
+              <FaBootstrap />
+              <h4>Bootstrap</h4>
+            </p>
+            <p className="item">
+              <SiTailwindcss />
+              <h4>Tailwind</h4>
+            </p>
+            <p className="item">
+              <FaHtml5 />
+              <h4>HTML</h4>
+            </p>
+            <p className="item">
+              <FaCss3 />
+              <h4>CSS</h4>
+            </p>
           </div>
-          <div className="card">
-            <h3>Backend Development</h3>
-            <div className="skills-list">
-              <p className="item">
-                <SiPython />
-                <h4>Python</h4>
-              </p>
-              <p className="item">
-                <IoLogoNodejs />
-                <h4>Node.js</h4>
-              </p>
-              <p className="item">
-                <SiExpress />
-                <h4>Express.js</h4>
-              </p>
-              <p className="item">
-                <SiMysql />
-                <h4>MySQL</h4>
-              </p>
-            </div>
+        </div>
+        <div
+          className="card"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <h3>Backend Development</h3>
+          <div className="skills-list">
+            <p className="item">
+              <SiPython />
+              <h4>Python</h4>
+            </p>
+            <p className="item">
+              <IoLogoNodejs />
+              <h4>Node.js</h4>
+            </p>
+            <p className="item">
+              <SiExpress />
+              <h4>Express.js</h4>
+            </p>
+            <p className="item">
+              <SiMysql />
+              <h4>MySQL</h4>
+            </p>
           </div>
         </div>
       </div>
